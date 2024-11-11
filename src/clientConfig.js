2e6bbc141b88062606215a544f7c28ae3e928748
sasha <sashalexjr@gmail.com>
2024-11-11 20:02:40 +0500

    hotfix

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -1,5 +1,5 @@
 const clientConfig = {
-    serverIP: 'localhost:3000', // ip бэкэнд сервера
+    serverIP: '85.192.26.15:3000', // ip бэкэнд сервера
     frontendIP: 'localhost:5173', // ip frontend сервера
     login: 'admin', //Логин для входа на сайт
     password: '77599557609', //Пароль для входа на сайт
